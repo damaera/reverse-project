97f1f9f3f8dd0599b580ab2fba953edd56807b11
Damaera <damaera@live.com>
2017-09-15 15:11:29 +0700

    adding transition and animation

--- public/js/script.js
+++ public/js/script.js
@@ -2,18 +2,32 @@ var obj = icaro({
   isLoading: false
 })
 
+function init() {
+  // setTimeout(function(){
+    document.querySelector('.content-opacity').style.opacity = 1
+    canvasAnimation()
+  // }, 1000)
+}
+
+init()
+
 obj.listen(function(changes) {
   var isLoading = changes.get('isLoading')
   var $tilt = document.querySelector('.tilt')
   if (isLoading) {
-    $tilt.vanillaTilt.destroy()
+    if ($tilt) {
+      $tilt.vanillaTilt.destroy()
+    }
   } else {
     VanillaTilt.init($tilt);
+    init()
   }
 })
 
+
 pageAccelerator({
   beforeLoading: function(e) {
+    document.querySelector('.content-opacity').style.opacity = 0
     obj.isLoading = true
   },
   afterLoading: function(e) {
