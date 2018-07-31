9ced653bdd832ac90da3e463fb67e3be6d1815a9
deronoitz <deronoitz@gmail.com>
2018-08-01 00:29:47 +0700

    group page

--- app/scripts/scripts.js
+++ app/scripts/scripts.js
@@ -1,5 +1,32 @@
 'use strict';
 
+var charThumb = $('ul.characterThumb li a');
+var groupCharImg = $('.groupChar img');
+var characterThumb = {
+  active: function active(e) {
+    var dataImg = e.attr('data-img');
+    if (dataImg) {
+      var target = $(".groupChar img#" + dataImg + "");
+      $(".groupChar div[contain='" + dataImg + "']").addClass('on');
+      TweenLite.to(groupCharImg, .225, { css: { opacity: .15, filter: "grayscale(1) blur(2.5px)" }, ease: Power2.easeInOut });
+      TweenLite.to(target, .225, { css: { opacity: 1, filter: "grayscale(0)" }, ease: Power2.easeInOut });
+    }
+  },
+  normalize: function normalize() {
+    $(".groupChar div[contain]").removeClass('on');
+    TweenLite.to(groupCharImg, .225, { css: { opacity: 1, filter: "grayscale(0) blur(0)" }, ease: Power2.easeInOut });
+  }
+};
+
+charThumb.on('mouseover', function () {
+  characterThumb.active($(this));
+});
+
+charThumb.on('mouseleave', function () {
+  characterThumb.normalize();
+});
+'use strict';
+
 var bgWrapper = $('.indexPage .bg-wrapper img');
 function getRandomFloat(min, max) {
   return Math.random() * (max - min) + min;
